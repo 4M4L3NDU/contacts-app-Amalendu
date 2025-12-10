4b192fba015f59b0f40c61efcd3eeb0229d783ad
Amalendu Kshithish <ambukan@gmail.com>
2025-12-10 05:44:06 +0000

    empty data structure

--- test.py
+++ test.py
@@ -1,1 +1,6 @@
-print ("hello")
+from contacts_app import ContactsManager, Contact
+
+def test_empty_structure():
+    manager = ContactsManager()
+    print("Test Empty Structure:")
+    manager.list_contacts()   # Expect: "No contacts found."
